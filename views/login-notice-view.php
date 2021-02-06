93fcdb56cd26624721e9a4973f016764a62be82b
Maria Akritidou <mariaak83@gmail.com>
2021-02-06 11:32:43 +0200

    Fix styles on login-notice-view

--- views/login-notice-view.php
+++ views/login-notice-view.php
@@ -4,14 +4,14 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-$login_url    = isset ( $options['login_url'] ) ? $options['login_url'] : wp_login_url();
-$register_url = isset ( $options['register_url'] ) ? $options['register_url'] : wp_registration_url();
-
+$login_url    	= isset ( $options['login_url'] ) ? $options['login_url'] : wp_login_url();
+$register_url 	= isset ( $options['register_url'] ) ? $options['register_url'] : wp_registration_url();
+$vln_custom_styles	= "background: {$options['primary_bg_color']}; background: -moz-linear-gradient(90deg, {$options['primary_bg_color']} 0%, {$options['secondary_bg_color']} 100%); background: -webkit-linear-gradient(90deg, {$options['primary_bg_color']} 0%, {$options['secondary_bg_color']} 100%); background: linear-gradient(90deg, {$options['primary_bg_color']} 0%, {$options['secondary_bg_color']} 100%); filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='{$options['primary_bg_color']}', endColorstr='{$options['secondary_bg_color']}',GradientType=1 );";
 ?>
 
 <?php if ( isset( $options['message'] ) ) { ?>
 
-<div id="visitor-login-notice" class="vln-<?php echo $options['position']; ?>" style="background: linear-gradient(90deg, <? echo $options['primary_bg_color']; ?>  0%, <? echo $options['secondary_bg_color']; ?> 100%);">
+<div id="visitor-login-notice" class="vln-<?php echo $options['position']; ?>" style="<?php echo $vln_custom_styles;?>" >
     <?php if ( isset ( $options['allow_close'] ) && $options['allow_close'] == '1' ) { ?>
 		<a href="#" id="vln-close-btn" aria-hidden="true"></a>
     <?php } ?>
@@ -33,16 +33,16 @@ $register_url = isset ( $options['register_url'] ) ? $options['register_url'] : 
 	<div class="vln-actions">
 
 		<a href="<?php echo esc_url( $options['login_url'] ); ?>"
-		   class="vln-btn vln-login-btn" style="background: <? echo $options['primary_btn_color']; ?>">
+		   class="vln-btn vln-login-btn" style="background: <?php echo $options['primary_btn_color']; ?>">
             <?php _e( 'Log In', 'login-notice' ); ?>
 		</a>
 
-		<a href="<?php echo esc_url( $options['register_url'] ); ?>" class="vln-btn vln-register-btn" style="background: <? echo $options['secondary_btn_color']; ?>">
+		<a href="<?php echo esc_url( $options['register_url'] ); ?>" class="vln-btn vln-register-btn" style="background: <?php echo $options['secondary_btn_color']; ?>">
             <?php _e( 'Sign Up', 'login-notice' ); ?>
 		</a>
 
         <?php if ( ! empty( $options['custom_text'] ) && ! empty ( $options['custom_url'] ) ) { ?>
-		<a href="<?php echo esc_url( $options['custom_url'] ); ?>" class="vln-btn vln-custom-btn" style="background: <? echo $options['secondary_btn_color']; ?>>
+		<a href="<?php echo esc_url( $options['custom_url'] ); ?>" class="vln-btn vln-custom-btn" style="background: <?php echo $options['secondary_btn_color']; ?>>
         <?php echo esc_attr( $options['custom_text'] ); ?>
 				</a>
         <?php } ?>
